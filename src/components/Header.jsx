4c93a229eb2237863a361ac0fc804bc9d5b2906b
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-05-03 18:07:13 +0000

    add lg and md screen styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Switcher from "./Switcher"
 
 function Header() {
   return (
-    <div className="mr-6 ml-6 mt-6 w-[327px]">
+    <div className="mr-6 ml-6 mt-6 w-[327px] md:w-[689px] md:mr-auto md:ml-auto lg:w-[736px]">
         <Switcher/>
     </div>
   )
