9d8b195f74265e6a0390e95e504ea367e03eac54
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-05-03 17:49:56 +0000

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Switcher from "./Switcher"
+
+function Header() {
+  return (
+    <div className="mr-6 ml-6 mt-6 w-[327px]">
+        <Switcher/>
+    </div>
+  )
+}
+
+export default Header
