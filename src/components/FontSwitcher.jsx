d943389db072062a44ea644ad55908da1bf0ffa9
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-05-03 17:56:44 +0000

    add media query

--- src/components/FontSwitcher.jsx
+++ src/components/FontSwitcher.jsx
@@ -2,7 +2,7 @@ import { IoIosArrowDown } from "react-icons/io";
 
 function FontSwitcher() {
   return (
-    <div className="flex items-center gap-4 w-14 text-sm font-mono font-bold text-black-20 leading-6">
+    <div className="flex items-center gap-4 w-14 md:w-[66px] lg:w-[70px] text-sm font-mono font-bold text-black-20 leading-6">
         <span>Mono</span>
         <IoIosArrowDown/>
     </div>
