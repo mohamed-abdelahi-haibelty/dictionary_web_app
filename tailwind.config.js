d096ff8c0f0246733f2455fe4c69520ba3594326
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-05-03 17:48:19 +0000

    add files

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        black:{
+          10:'#3A3A3A',
+          20:'#2D2D2D',
+          30:`#1F1F1F`,
+          40:'#050505'
+        },    
+        gray:{
+          10:'#F4F4F4',
+          20:'#E9E9E9',
+          30:`#757575`,
+        },
+        primary:'#A445ED',
+        secondry:'#FF5252'    
+      },
+      fontFamily:{
+        mono:['Inconsolata', 'monospace'],
+        serif: ['Lora', 'serif'],
+        sansSerif:['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
+
