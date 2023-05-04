8896f46aefde2abd202ee8810f9a56bd5468ec09
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-05-04 10:04:21 +0000

    change font dynimicaly

--- src/components/FontSwitcher.jsx
+++ src/components/FontSwitcher.jsx
@@ -1,11 +1,15 @@
 import { IoIosArrowDown } from "react-icons/io";
+import { fontContext } from '../context/FontContext';
+import { useContext } from 'react';
+
 
 function FontSwitcher() {
+  const {fontType} = useContext(fontContext)
   return (
-    <div className="flex dark:text-white items-center gap-4 w-14 md:w-[66px] lg:w-[70px] text-sm md:text-lg font-mono font-bold text-black-20 leading-6">
-        <span>Mono</span>
+      <div className={`relative flex dark:text-white items-center gap-4 text-sm md:text-lg font-${fontType.value} font-bold text-black-20 leading-6`}>
+        <span>{fontType.text}</span>
         <IoIosArrowDown className="dark:text-primary"/>
-    </div>
+      </div>
   )
 }
 
