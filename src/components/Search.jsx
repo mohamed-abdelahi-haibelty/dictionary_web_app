93f7ff8071c89e8dbf6cec94cdb75f8798c1271c
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-05-05 10:30:38 +0000

    add dark mode style to search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,8 +4,8 @@ function Search() {
   const fontType = JSON.parse(localStorage.fontType)
   return (
     <div>
-        <div className="w-[327px] md:w-[689px] lg:w-[736px] relative px-6 py-4 md:py-[22px] lg:py-[19px] flex items-center justify-between bg-gray-10 rounded-2xl">
-            <input type="text" className={`w-full font-${fontType} cursor-pointer outline-none pr-6 bg-transparent text-black-20 font-bold text-base md:text-xl placeholder-black-20/25`} placeholder='Search for any word...'/>
+        <div className="w-[327px] dark:bg-black-30 md:w-[689px] lg:w-[736px] relative px-6 py-4 md:py-[22px] lg:py-[19px] flex items-center justify-between bg-gray-10 rounded-2xl">
+            <input type="text" className={`w-full font-${fontType} dark:text-white cursor-pointer outline-none pr-6 bg-transparent text-black-20 font-bold text-base md:text-xl placeholder-black-20/25 dark:placeholder-white/25`} placeholder='Search for any word...'/>
             <CiSearch className='text-primary absolute w-4 h-4 right-6 pointer-events-none'/>
         </div>
     </div>
