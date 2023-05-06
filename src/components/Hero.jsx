a0f5a7422e5a610e155d9cc07d13e612e3e9d169
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-05-06 08:49:46 +0000

    add hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,25 @@
 import Head from "./Head"
 import { useContext } from 'react';
 import { fontContext } from '../context/FontContext';
+import Translate from "./Translate";
+import Source from "./Source";
 
 function Hero() {
   const {data} = useContext(fontContext)
+  const {meanings, sourceUrls, word} = data.length > 0 && data[0]
   return (
     <div className='mr-auto ml-auto mt-7 w-[327px] md:w-[737px]'>
         {data.length > 0 && <Head/>}
+        {data.length > 0 && meanings.map((m,i) => 
+        <Translate 
+            key={i} 
+            partOfSpeech={m.partOfSpeech}
+            definitions={m.definitions}
+            synonyms={m.synonyms}  
+        />
+        )}
+        {data.length > 0 && <Source sourceUrls={sourceUrls} word={word}/>}
+        
     </div>
   )
 }
